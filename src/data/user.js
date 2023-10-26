353fc721ee4b31ea818bd570e46a40cda9b55fdd
SengokuGarp <garpirungu@gmail.com>
2023-10-26 17:46:28 +0300

    Completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -3,4 +3,8 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
+console.log(username);
+console.log(city);
+console.log(image);
+
 // export all three variables as named exports
